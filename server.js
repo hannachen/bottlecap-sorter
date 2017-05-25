547ebd9ecce2d939e3c2bd7dea742aafabee95bd
Hanna Chen <hanna@rethinkcanada.com>
2017-05-25 01:43:24 -0400

    List images

--- server.js
+++ server.js
@@ -12,9 +12,37 @@ var camera = new RaspiCam({
 })
 camera.start()
 
+var imageDir = '/var/www/camera/images/'
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello World  <img src="./snapshot.jpg" />\n');
+  //use the url to parse the requested url and get the image name
+  var query = url.parse(req.url,true).query;
+  pic = query.image;
+
+  if (typeof pic === 'undefined') {
+    getImages(imageDir, function (err, files) {
+      var imageLists = '<ul>';
+      for (var i=0; i<files.length; i++) {
+        imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
+      }
+      imageLists += '</ul>';
+      res.writeHead(200, {'Content-type':'text/html'});
+      res.end(imageLists);
+    });
+  } else {
+    //read the image using fs and send the image content back in the response
+    fs.readFile(imageDir + pic, function (err, content) {
+      if (err) {
+        res.writeHead(400, {'Content-type':'text/html'})
+        console.log(err);
+        res.end("No such image");
+      } else {
+        //specify the content type in the response will be an image
+        res.writeHead(200,{'Content-type':'image/jpg'});
+        res.end(content);
+      }
+    });
+  }
+
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 })
