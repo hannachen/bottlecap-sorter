0e0b0c261fbd8fa865b8344c5036a66483343194
Hanna Chen <hanna@rethinkcanada.com>
2017-05-25 01:53:09 -0400

    Add list images function

--- server.js
+++ server.js
@@ -15,6 +15,20 @@ var camera = new RaspiCam({
 })
 camera.start()
 
+//get the list of jpg files in the image dir
+function getImages(imageDir, callback) {
+  var fileType = '.jpg',
+    files = [], i;
+  fs.readdir(imageDir, function (err, list) {
+    for(i=0; i<list.length; i++) {
+      if(path.extname(list[i]) === fileType) {
+        files.push(list[i]); //store the file name into the array files
+      }
+    }
+    callback(err, files);
+  });
+}
+
 var imageDir = '/var/www/camera/images/'
 http.createServer((req, res) => {
   //use the url to parse the requested url and get the image name
