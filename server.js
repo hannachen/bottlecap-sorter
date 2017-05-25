f5c0eae7a6b5ced52652dea54b235640f35701e6
Hanna Chen <hanna@rethinkcanada.com>
2017-05-25 01:28:04 -0400

    Test raspicam

--- server.js
+++ server.js
@@ -1,45 +1,15 @@
-const http = require('http');
-const Raspi = require('raspi-io');
-const five = require('johnny-five');
-const NodeWebcam = require('node-webcam');
+const http = require('http')
+const Raspi = require('raspi-io')
+const five = require('johnny-five')
+const RaspiCam = require('raspicam')
 
 const hostname = '0.0.0.0'; // listen on all ports
 const port = 80;
 
-var opts = {
-  //Picture related
-  width: 1280,
-  height: 720,
-  quality: 100,
-
-  //Delay to take shot
-  delay: 0,
-
-  //Save shots in memory
-  saveShots: true,
-
-  // [jpeg, png] support varies
-  // Webcam.OutputTypes
-  output: "jpeg",
-
-  //Which camera to use
-  //Use Webcam.list() for results
-  //false for default device
-  device: false,
-
-  // [location, buffer, base64]
-  // Webcam.CallbackReturnTypes
-  callbackReturn: "location",
-
-  //Logging
-  verbose: false
-}
-
-var Webcam = NodeWebcam.create(opts);
-Webcam.capture('test_picture', function(err, data) {
-  console.log(err)
-  console.log(data)
+var camera = new RaspiCam({
+  mode: 'photo'
 });
+camera.start();
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
