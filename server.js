a986e2d81c05f2e6e9401066dd84312b97c1449d
Hanna Chen <hanhan@gmail.com>
2017-05-25 01:50:53 -0400

    Fix js errors

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const url = require('url')
 const Raspi = require('raspi-io')
 const five = require('johnny-five')
 const RaspiCam = require('raspicam')
