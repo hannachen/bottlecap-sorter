4cd364a18fc8df57fb48acc26167bacb204517b6
Hanna Chen <hanna@rethinkcanada.com>
2017-05-30 00:49:19 -0400

    Add button and refactor

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const RaspiCam = require('raspicam')
 const hostname = '0.0.0.0'; // listen on all ports
 const port = 80;
 
+const servoMin = -30
+const servoMax = 180
+
 var camera = new RaspiCam({
   mode: 'photo',
   output: 'snapshots/snapshot.jpg'
 })
-camera.start()
 
 //get the list of jpg files in the image dir
 function getImages(imageDir, callback) {
   var fileType = '.jpg',
-    files = [], i;
+      files = [], i;
   fs.readdir(imageDir, function (err, list) {
     for(i=0; i<list.length; i++) {
       if(path.extname(list[i]) === fileType) {
@@ -26,42 +28,42 @@ function getImages(imageDir, callback) {
       }
     }
     callback(err, files);
-  });
+  })
 }
 
 var imageDir = '/var/www/camera/snapshots/'
 http.createServer((req, res) => {
   //use the url to parse the requested url and get the image name
-  var query = url.parse(req.url,true).query;
-  pic = query.image;
+  var query = url.parse(req.url,true).query
+  var pic = query.image
 
   if (typeof pic === 'undefined') {
     getImages(imageDir, function (err, files) {
-      var imageLists = '<ul>';
+      var imageLists = '<ul>'
       for (var i=0; i<files.length; i++) {
-        imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>';
+        imageLists += '<li><a href="/?image=' + files[i] + '">' + files[i] + '</li>'
       }
-      imageLists += '</ul>';
-      res.writeHead(200, {'Content-type':'text/html'});
-      res.end(imageLists);
+      imageLists += '</ul>'
+      res.writeHead(200, {'Content-type':'text/html'})
+      res.end(imageLists)
     });
   } else {
     //read the image using fs and send the image content back in the response
     fs.readFile(imageDir + pic, function (err, content) {
       if (err) {
         res.writeHead(400, {'Content-type':'text/html'})
-        console.log(err);
-        res.end("No such image");
+        console.log(err)
+        res.end("No such image")
       } else {
         //specify the content type in the response will be an image
-        res.writeHead(200,{'Content-type':'image/jpg'});
-        res.end(content);
+        res.writeHead(200, {'Content-type':'image/jpg'})
+        res.end(content)
       }
     });
   }
 
 }).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
 
 const board = new five.Board({
@@ -73,10 +75,10 @@ board.on('ready', () => {
   // Create servo on pin 1 (GPIO18) on P1
   var servo = new five.Servo({
     pin: 1,
-    range: [-30, 180],
+    range: [servoMin, servoMax],
     center: true
-  });
-  servo.center();
+  })
+  servo.center()
 
   var relay = new five.Relay({
     pin: 22
@@ -85,38 +87,51 @@ board.on('ready', () => {
   var button = new five.Button({
     pin: 25,
     isPullup: true
-  });
+  })
 
   button.on('up', function() {
-    relay.off();
-    console.log('button up');
-  });
+    relay.off()
+    console.log('button up')
+  })
 
   button.on('down', function() {
-    relay.on();
-    console.log('button down');
+    relay.on()
+    console.log('button down')
   })
 
   var button2 = new five.Button({
     pin: 4,
     isPullup: true
-  });
+  })
 
-  var toggle = false;
-  button2.on('up', function(value) {
+  var toggle = false
+  button2.on('up', function() {
     if (toggle) {
-      servo.to(180);
-      console.log('to 180');
+      servo.to(servoMin)
+      console.log(`Servo to ${servoMin}`)
     } else {
-      servo.to(-30);
-      console.log('to -30');
+      servo.to(servoMax)
+      console.log(`Servo to ${servoMax}`)
     }
     toggle = !toggle;
-    console.log('button2 up');
-  });
+    console.log('button2 up')
+  })
 
   button2.on('down', function() {
-    console.log('button2', toggle);
-  });
+    console.log('button2', toggle)
+  })
+
+  var button3 = new five.Button({
+    pin: 27,
+    isPullup: true
+  })
+
+  button3.on('down', function() {
+  })
+
+  button2.on('up', function() {
+    console.log('button3 up, taking photo')
+    camera.start()
+  })
 
 });
